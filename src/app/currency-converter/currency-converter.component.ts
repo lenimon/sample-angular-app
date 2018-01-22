55728b5340b045f80d8156a55f28a8c20c73134a
Lenins George <leninmon@gmail.com>
2018-01-22 21:05:26 +0530

    Added number field validations and added loading mask in currency converter component.

--- src/app/currency-converter/currency-converter.component.ts
+++ src/app/currency-converter/currency-converter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CurrencyMainService } from 'app/currency-converter/main.service';
 
 @Component({
@@ -14,19 +14,25 @@ export class CurrencyConverterComponent implements OnInit {
   data:any;
   selectedBaseCurrency: String="CAD";
   selectedToCurrency: String="CAD";
+  @ViewChild("spinner",{read: ElementRef}) spinner: ElementRef;
 
   constructor(private mainService: CurrencyMainService) { }
 
   ngOnInit() {
+    this.spinner.nativeElement.hidden = false;
     this.mainService.getLatestConversionValues(this.selectedBaseCurrency, this.baseCurrencies).subscribe(data=> { 
       this.data=data;
+      this.spinner.nativeElement.hidden = true;
     });
+
   }
 
   onSelectBaseCurrencyType(value, fromField, toField){
     this.selectedBaseCurrency=value;
+    this.spinner.nativeElement.hidden = false;
     this.mainService.getLatestConversionValues(this.selectedBaseCurrency, this.baseCurrencies).subscribe(data=> { 
       this.data=data;
+      this.spinner.nativeElement.hidden = true;
       this.onCurrencyValueChange(fromField, this.selectedToCurrency, toField);
     });
   }
@@ -36,6 +42,10 @@ export class CurrencyConverterComponent implements OnInit {
   }
 
   onCurrencyValueChange(fromField, toCurrency, toField){
+    if(isNaN(fromField.value) || fromField.value<=0 || event["key"]=="."){
+      fromField.value = fromField.value.substr(0,fromField.value.length-1);
+    }
+
     if(this.selectedBaseCurrency==this.selectedToCurrency){
       toField.value=fromField.value;
     }else if(this.data.rates[toCurrency]){
@@ -44,4 +54,5 @@ export class CurrencyConverterComponent implements OnInit {
       toField.value=null;
     }
   }
+
 }
